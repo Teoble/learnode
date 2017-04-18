163e93eb8510ef352f188e9b72b2b5074548ae81
Rafael Pereira <rafael.pereira.sjbv@gmail.com>
2017-04-17 23:35:29 -0300

    Lição 3

--- program.js
+++ program.js
@@ -1,6 +1,5 @@
 var dblSum;
 dblSum = 0;
-for(intI = 2; intI < process.argv.length; intI++){
+for(intI = 2; intI < process.argv.length; intI++)
   dblSum = dblSum + Number(process.argv[intI]);
-}
 console.log(dblSum);
